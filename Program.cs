7983c0ae0274bcf9e0fb9b24d2ea04ca6e958be6
Bazoo <mathieu77197@gmail.com>
2020-11-22 13:07:42 -0500

    add base files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lingos_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
