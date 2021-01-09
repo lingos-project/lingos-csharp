0934ee0aa82687f374aa028616cdbf8d477a1dbf
Bazoo <mathieu77197@gmail.com>
2021-01-09 18:52:57 -0500

    Fix zipping including folders

--- scripts/createZipFolders.sh
+++ scripts/createZipFolders.sh
@@ -4,5 +4,5 @@ for d in "$1"/*/ ; do
   folder_path=$(readlink -f "$d");
   basename=$(basename "$folder_path");
   mkdir $2;
-  zip -r "$2/$basename.zip" "$folder_path" ;
+  zip "$2/$basename.zip" "$folder_path";
 done
