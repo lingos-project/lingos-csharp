66c6912784bb97c42cc54dd64ec1340c4cbadbde
Mathieu Chan <Bazooo@users.noreply.github.com>
2021-01-09 18:36:21 -0500

    Try fixing release workflow (#2)

--- scripts/createZipFolders.sh
+++ scripts/createZipFolders.sh
@@ -3,5 +3,6 @@
 for d in "$1"/*/ ; do
   folder_path=$(readlink -f "$d");
   basename=$(basename "$folder_path");
-  zip -r "$2/$basename.zip" "$folder_path" 
+  mkdir $2;
+  zip -r "$2/$basename.zip" "$folder_path" ;
 done
