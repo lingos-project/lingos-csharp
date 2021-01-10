ab9f9242f418261afeef1988d537acbc83cb05e3
Bazoo <mathieu77197@gmail.com>
2021-01-10 14:16:41 -0500

    Update release workflow

--- scripts/createZipFolders.sh
+++ scripts/createZipFolders.sh
@@ -3,6 +3,6 @@
 for d in "$1"/*/ ; do
   folder_path=$(readlink -f "$d");
   basename=$(basename "$folder_path");
-  mkdir $2;
-  zip "$2/$basename.zip" "$folder_path";
+  mkdir "$2";
+  7z a "$2/$basename.zip" "$folder_path";
 done
